a8d5ee3c6fcea02bbe40b55cb16e39740cea0337
uniqpirlo <huang.pirlo@gmail.com>
2018-06-16 17:02:05 +0900

    japanese problem fixed

--- hello.py
+++ hello.py
@@ -2,19 +2,22 @@
 # coding:utf-8
 
 import subprocess
+import sys
+import io
+sys.stdout=io.TextIOWrapper(sys.stdout.buffer, encoding='utf-8')
 
 cmd='ls /home/hzh'
 
 results=subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE).stdout.readlines()
 
-print('Content-Type: text/html;\n\n')
+print('Content-Type: text/html;charset=UTF-8\n\n')
 print("<!DOCTYPE html>")
 print("<html lang='ja'>")
 print("<body>")
 print("    <h1>Hello world.</h1>")
 print('<table border="1">')
 print("<tr><th>File name</th></tr>")
-[print("<tr><td>{}</td></tr>".format(line)) for line in results]
+[print("<tr><td>{}</td></tr>".format(line.decode('utf-8'))) for line in results]
 print("</table>")
 print("</body>")
 print("</html>")
